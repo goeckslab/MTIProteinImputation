bc964d6ad6f1c5d2ee987f62a23431eeff2282ca
Exitare <business.exitare@icloud.com>
2024-03-20 11:18:57 -0700

    Update treatment_classifier.py

--- src/classifier/treatment_classifier.py
+++ src/classifier/treatment_classifier.py
@@ -1,14 +1,69 @@
-import argparse, sys
+import argparse, sys, os
 from pathlib import Path
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import MinMaxScaler
 
 SHARED_MARKERS = ['pRB', 'CD45', 'CK19', 'Ki67', 'aSMA', 'Ecad', 'PR', 'CK14', 'HER2', 'AR', 'CK17', 'p21', 'Vimentin',
                   'pERK', 'EGFR', 'ER']
+PATIENTS = ["9_2", "9_3", "9_14", "9_15"]
 
 save_path: Path = Path("results", "classifier")
+
+
+def clean_column_names(df: pd.DataFrame):
+    if "ERK-1" in df.columns:
+        # Rename ERK to pERK
+        df = df.rename(columns={"ERK-1": "pERK"})
+
+    if "E-cadherin" in df.columns:
+        df = df.rename(columns={"E-cadherin": "Ecad"})
+
+    if "Rb" in df.columns:
+        df = df.rename(columns={"Rb": "pRB"})
+
+    return df
+
+
+def load_imputed_data() -> dict:
+    imputed_data = {}
+    for patient in PATIENTS:
+        pre_treatment_path = Path("data", "ae_reconstructed_data", mode, patient, f"{radius}", "pre_treatment.csv")
+        on_treatment_path = Path("data", "ae_reconstructed_data", mode, patient, f"{radius}", "on_treatment.csv")
+
+        if not pre_treatment_path.exists() or not on_treatment_path.exists():
+            print(f"Pre or on treatment file for {patient} does not exist.")
+            sys.exit(1)
+
+        # load files
+        pre_treatment = pd.read_csv(pre_treatment_path)
+        on_treatment = pd.read_csv(on_treatment_path)
+
+        # combine data
+        combined_data = pd.concat([pre_treatment, on_treatment])
+        imputed_proteins = combined_data[SHARED_MARKERS]
+        imputed_data[patient] = imputed_proteins
+    return imputed_data
+
+
+def load_train_data(base_path: Path, patient: str):
+    train_data = []
+    for file in os.listdir(base_path):
+        file_name = Path(file).stem
+        if file.endswith(".csv") and patient not in file_name:
+            print("Loading train file: " + file)
+            data = pd.read_csv(Path(base_path, file))
+            data = clean_column_names(data)
+            data["Treatment"] = "PRE" if "_1" in file else "ON"
+
+            assert "Treatment" in data.columns, f"Treatment column is missing for dataframe of patient {file}"
+            train_data.append(data)
+
+    assert len(train_data) == 6, f"There should be 6 train datasets, loaded {len(train_data)}"
+    return pd.concat(train_data)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -30,65 +85,75 @@ if __name__ == '__main__':
     if not save_path.exists():
         save_path.mkdir(parents=True)
 
-    # load patient data from the ae single imputation folder
-    pre_treatment_path = Path("data", "ae_reconstructed_data", mode, patient, f"{radius}", "pre_treatment.csv")
-    on_treatment_path = Path("data", "ae_reconstructed_data", mode, patient, f"{radius}", "on_treatment.csv")
+    train_data = load_train_data(Path("data", "bxs"), patient)
 
-    if not pre_treatment_path.exists() or not on_treatment_path.exists():
-        print("Pre or on treatment file does not exist.")
-        sys.exit(1)
+    pre_treatment_test = pd.read_csv(Path("data", "bxs", f"{patient}_1.csv"))
+    pre_treatment_test = clean_column_names(pre_treatment_test)
+    pre_treatment_test["Treatment"] = "PRE"
 
-    # load files
-    pre_treatment = pd.read_csv(pre_treatment_path)
-    on_treatment = pd.read_csv(on_treatment_path)
+    on_treatment_test = pd.read_csv(Path("data", "bxs", f"{patient}_2.csv"))
+    on_treatment_test = clean_column_names(on_treatment_test)
+    on_treatment_test["Treatment"] = "ON"
+    test_data = pd.concat([pre_treatment_test, on_treatment_test])
 
-    # combine data
-    combined_data = pd.concat([pre_treatment, on_treatment])
+    train_data_proteins = train_data[SHARED_MARKERS]
+    test_data_proteins = test_data[SHARED_MARKERS]
 
-    imputed_proteins = combined_data[SHARED_MARKERS]
+    train_data_treatment = train_data["Treatment"]
+    test_data_treatment = test_data["Treatment"]
 
-    # load original data
-    original_data_pre = pd.read_csv(Path("data", "bxs", f"{patient}_1.csv"))
-    original_data_on = pd.read_csv(Path("data", "bxs", f"{patient}_2.csv"))
-    # add treatment column
-    original_data_pre["Treatment"] = "PRE"
-    original_data_on["Treatment"] = "ON"
+    imputed_data = load_imputed_data()
+    assert len(imputed_data) == 4, "Imputed data should have 4 patients"
 
-    original_data = pd.concat([original_data_pre, original_data_on])
+    # all patients except the patient
+    train_imputed_data = pd.concat(
+        [imputed_data[current_patient] for current_patient in imputed_data if current_patient != patient])
+    test_imputed_data = imputed_data[patient]
 
-    # get the proteins
-    original_proteins = original_data[SHARED_MARKERS].copy()
-    original_treatment = original_data["Treatment"].copy()
+    # check that train_imputed data shape and train_data_protein shape is similar
+    assert train_imputed_data.shape == train_data_proteins.shape, "Train imputed data shape is not correct"
 
     scores = []
-    for target_protein in imputed_proteins:
+    for target_protein in test_data_proteins.columns:
         print(f"Working on protein {target_protein}...")
-
-        original_proteins[target_protein] = imputed_proteins[target_protein].values
-
-        # create train test split
-        X_train, X_test, y_train, y_test = train_test_split(original_proteins, original_treatment,
-                                                            test_size=0.2, random_state=42)
 
         # scale the data
         scaler = MinMaxScaler()
-        X_train = scaler.fit_transform(X_train)
-        X_test = scaler.transform(X_test)
+        X_train = scaler.fit_transform(train_data_proteins)
+        X_test = scaler.transform(test_data_proteins)
 
         # train classifier on masked data using random forest using sklearn
-        model = RandomForestClassifier()
-        model.fit(X_train, y_train)
-        imputed_score = model.score(X_test, y_test)
+        model = DecisionTreeClassifier()
+        model.fit(X_train, train_data_treatment)
+        original_score = model.score(X_test, test_data_treatment)
+        print(f"Score for protein {target_protein} using original data: {original_score}")
 
+        train_data_proteins[target_protein] = train_imputed_data[target_protein].values
+
+        # check that train data target protein values are different from the original train data
+        assert not train_data_proteins[target_protein].equals(train_data[target_protein]), \
+            "Train data target protein values are the same as the original train data"
+
+        # scale the data
+        scaler = MinMaxScaler()
+        X_train = scaler.fit_transform(train_data_proteins)
+        X_test = scaler.transform(test_data_proteins)
+
+        # train classifier on masked data using random forest using sklearn
+        model = DecisionTreeClassifier()
+        model.fit(X_train, train_data_treatment)
+        imputed_score = model.score(X_test, test_data_treatment)
         print(f"Score for protein {target_protein} using imputed data: {imputed_score}")
 
-        # create train test split using original data, but remove target protein
-        X_train, X_test, y_train, y_test = train_test_split(original_proteins.drop(columns=[target_protein]),
-                                                            original_treatment,
-                                                            test_size=0.2, random_state=42)
+        # remove target protein from proteins
+        X_train = train_data_proteins.drop(columns=[target_protein])
+        X_test = test_data_proteins.drop(columns=[target_protein])
 
         # shape of X_train and X_test should be one less that the original_proteins
-        assert X_train.shape[1] == original_proteins.shape[1] - 1, "X_train shape is not correct"
+        assert X_train.shape[1] == train_data_proteins.shape[1] - 1, "X_train shape is not correct"
+        assert X_test.shape[1] == test_data_proteins.shape[1] - 1, "X_test shape is not correct"
+        # check that in x train only SHARED_MARKERS are present
+        assert all([protein in SHARED_MARKERS for protein in X_train.columns]), "X_train contains other proteins"
 
         scaler = MinMaxScaler()
         X_train = scaler.fit_transform(X_train)
@@ -96,13 +161,14 @@ if __name__ == '__main__':
 
         # train classifier on original data using random forest using sklearn
         model = RandomForestClassifier()
-        model.fit(X_train, y_train)
-        original_score = model.score(X_test, y_test)
+        model.fit(X_train, train_data_treatment)
+        removed_score = model.score(X_test, test_data_treatment)
 
-        print(f"Score for protein {target_protein} using original data: {original_score}")
+        print(f"Score for protein {target_protein} using data with removed protein: {removed_score}")
 
         # save the scores
-        scores.append({"Protein": target_protein, "Imputed Score": imputed_score, "Original Score": original_score})
+        scores.append({"Protein": target_protein, "Imputed Score": imputed_score, "Original Score": original_score,
+                       "Removed score": removed_score})
 
     scores = pd.DataFrame(scores)
     scores.to_csv(Path(save_path, "classifier_scores.csv"), index=False)
