b8cd74faccf7eb2723a2cfb6377dbcedb049c0b8
Exitare <business.exitare@icloud.com>
2024-07-10 10:41:59 -0700

    Update null model

--- src/null_model/null_model.py
+++ src/null_model/null_model.py
@@ -1,8 +1,11 @@
 import pandas as pd
 from pathlib import Path
-import argparse
 import os
 from tqdm import tqdm
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_absolute_error, root_mean_squared_error
+import numpy as np
+from sklearn.linear_model import ElasticNetCV
 
 BIOPSIES = ["9_2_1", "9_2_2", "9_3_1", "9_3_2", "9_14_1", "9_14_2", "9_15_1", "9_15_2"]
 SHARED_MARKERS = ['pRB', 'CD45', 'CK19', 'Ki67', 'aSMA', 'Ecad', 'PR', 'CK14', 'HER2', 'AR', 'CK17', 'p21', 'Vimentin',
@@ -43,8 +46,6 @@ if __name__ == '__main__':
     if not save_path.exists():
         save_path.mkdir(parents=True)
 
-    parser = argparse.ArgumentParser(description='Run null model')
-    args = parser.parse_args()
     scores = []
     for biopsy in BIOPSIES:
         print("Working on biopsy: ", biopsy)
@@ -52,28 +53,39 @@ if __name__ == '__main__':
         # load biopsy data
         test_data = pd.read_csv(Path("data", "bxs", f"{biopsy}.csv"))
         train_data = load_train_data(Path("data", "bxs"), patient=patient)
+
         for protein in SHARED_MARKERS:
             for i in tqdm(range(30)):
-                # use the mean to impute the values
-                #y_hat = train_data.sample(n=len(test_data), replace=True)[protein]
-                y_hat = train_data[protein].sample(n=len(test_data), replace=True).mean()
+                temp_train_data = train_data[SHARED_MARKERS].copy()
+                temp_train_data = temp_train_data.drop(protein, axis=1)
+                temp_test_data = test_data[SHARED_MARKERS].copy()
+                temp_test_data = temp_test_data.drop(protein, axis=1)
+
+                ground_truth = test_data[protein]
+                y_hat = train_data[protein].sample(frac=0.8).mean()
+                y_hat = np.repeat(y_hat, len(ground_truth))
+
+                # run elastic net using ElasticNetCv from sklearn
+                model = ElasticNetCV(l1_ratio=[.1, .5, .7, .9, .95, .99, 1], cv=5, n_jobs=-1, max_iter=10000)
+
+                model.fit(temp_train_data, train_data[protein])
+                en_y_hat = model.predict(temp_test_data)
+
+                en_mae = mean_absolute_error(en_y_hat, ground_truth)
+                en_rmse = root_mean_squared_error(en_y_hat, ground_truth)
 
                 # calculate mae, rmse
-                mae = (y_hat - test_data[protein]).abs().mean()
-                rmse = ((y_hat - test_data[protein]) ** 2).mean() ** .5
+                mae = mean_absolute_error(y_hat, ground_truth)
+                rmse = root_mean_squared_error(y_hat, ground_truth)
 
-                # print(f"Biopsy {biopsy}, Protein {protein}, MAE: {mae}, RMSE: {rmse}")
-                scores.append({"Biopsy": biopsy, "Protein": protein, "MAE": mae, "RMSE": rmse, "Iteration": i})
+                scores.append(
+                    {"Biopsy": biopsy, "Protein": protein, "MAE": en_mae, "RMSE": en_rmse, "Iteration": i,
+                     "Model": "EN"})
+                scores.append(
+                    {"Biopsy": biopsy, "Protein": protein, "MAE": mae, "RMSE": rmse, "Iteration": i, "Model": "Null"})
 
     scores = pd.DataFrame(scores)
 
     # sort scores by protein
     scores = scores.sort_values(by=["Protein", "Biopsy"])
     scores.to_csv(Path(save_path, "scores.csv"), index=False)
-
-    # plot results
-    # sns.set_theme(style="whitegrid")
-    # sns.set_context("paper")
-    # fig, ax = plt.subplots(1, 2, figsize=(10, 5))
-    # sns.boxplot(data=scores, x="Protein", y="MAE", ax=ax[0])
-    # sns.boxplot(data=scores, x="Protein", y="RMSE", ax=ax[1])
