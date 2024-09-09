9dc046a5526ee9865e216b2caec5fafe51f1366c
Exitare <business.exitare@icloud.com>
2024-09-09 10:39:06 -0700

    Update fig.2 to include ARI & Silhouette scores

--- src/figures/fig2.py
+++ src/figures/fig2.py
@@ -1,57 +1,44 @@
 import warnings
-
-warnings.simplefilter(action='ignore', category=FutureWarning)
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 from pathlib import Path
 import sys
-from typing import List
 from statannotations.Annotator import Annotator
 
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 image_folder = Path("figures", "fig2")
+PATIENTS = ["9_2", "9_3", "9_14", "9_15"]
 SHARED_PROTEINS = ['pRB', 'CD45', 'CK19', 'Ki67', 'aSMA', 'Ecad', 'PR', 'CK14', 'HER2', 'AR', 'CK17', 'p21', 'Vimentin',
                    'pERK', 'EGFR', 'ER']
-PATIENTS = ["9_2", "9_3", "9_14", "9_15"]
 
 
-def create_bar_plot_null_model(data: pd.DataFrame, metric: str) -> plt.Figure:
+# Function for creating the bar plot for Null vs EN models
+def create_bar_plot_null_model(data: pd.DataFrame, metric: str, ax=None) -> plt.Figure:
     hue = "Model"
     ax = sns.barplot(data=data, x="Marker", y=metric, hue=hue, hue_order=["Null", "EN"],
-                     palette={"EN": "lightblue", "Null": "red"})
+                     palette={"EN": "lightblue", "Null": "red"}, ax=ax)
 
-    # Set y axis to log scale
-    # ax.set_yscale('log', base=10)
-
-    # Optional: Set title and remove axis labels if needed
-    ax.set_ylabel("")
-    ax.set_xlabel("")
-
-    # Set y axis limits
     ax.set_ylim(0, 0.6)
-
-    # Reduce font size of x and y ticks
     ax.tick_params(axis='both', which='major', labelsize=8)
+    ax.legend(bbox_to_anchor=[0.6, 0.85], loc='center', ncol=2)
 
-    # Remove box around the plot
+    for label in ax.get_xticklabels():
+        if label.get_text() == 'Mean':
+            label.set_fontstyle('italic')
+            label.set_fontweight('bold')
+
+    ax.set_xticklabels(
+        ['Mean\nof all\nproteins' if x.get_text() == 'Mean' else x.get_text() for x in ax.get_xticklabels()])
+
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.spines['left'].set_visible(False)
     ax.spines['bottom'].set_visible(False)
 
-    # Adjust legend position
-    ax.legend(bbox_to_anchor=[0.6, 0.85], loc='center', ncol=2)
-
-    # Change font style of the 'mean' column
-    for label in ax.get_xticklabels():
-        if label.get_text() == 'Mean':
-            label.set_fontstyle('italic')
-            label.set_fontweight('bold')
-    # rename mean column to Mean of all proteins
-    ax.set_xticklabels(
-        ['Mean\nof all\nproteins' if x.get_text() == 'Mean' else x.get_text() for x in ax.get_xticklabels()])
-
-    hue_order = ["Null", "EN"]
+    # Add statistical annotations
     pairs = [
         (("pRB", "Null"), ("pRB", "EN")),
         (("CD45", "Null"), ("CD45", "EN")),
@@ -69,12 +56,11 @@ def create_bar_plot_null_model(data: pd.DataFrame, metric: str) -> plt.Figure:
         (("pERK", "Null"), ("pERK", "EN")),
         (("EGFR", "Null"), ("EGFR", "EN")),
         (("ER", "Null"), ("ER", "EN")),
-        (("Mean", "Null"), ("Mean", "EN")),
+        (("Mean", "Null"), ("Mean", "EN"))
     ]
-    order = ['pRB', 'CD45', 'CK19', 'Ki67', 'aSMA', 'Ecad', 'PR', 'CK14', 'HER2', 'AR', 'CK17', 'p21', 'Vimentin',
-             'pERK', 'EGFR', 'ER', "Mean"]
-    annotator = Annotator(ax, pairs, data=data, x="Marker", y=metric, order=order, hue=hue, hue_order=hue_order,
-                          verbose=1)
+
+    order = SHARED_PROTEINS + ["Mean"]
+    annotator = Annotator(ax, pairs, data=data, x="Marker", y=metric, order=order, hue=hue, hue_order=["Null", "EN"])
     annotator.configure(test='Mann-Whitney', text_format='star', loc='outside',
                         comparisons_correction="Benjamini-Hochberg")
     annotator.apply_and_annotate()
@@ -82,40 +68,29 @@ def create_bar_plot_null_model(data: pd.DataFrame, metric: str) -> plt.Figure:
     return ax
 
 
-def create_bar_plot_en_vs_lgbm(data: pd.DataFrame, metric: str) -> plt.Figure:
+# Function for creating the bar plot for EN vs LGBM models
+def create_bar_plot_en_vs_lgbm(data: pd.DataFrame, metric: str, ax=None) -> plt.Figure:
     ax = sns.barplot(data=data, x="Marker", y=metric, hue="Network", hue_order=["EN", "LGBM"],
-                     palette={"EN": "lightblue", "LGBM": "orange"})
+                     palette={"EN": "lightblue", "LGBM": "orange"}, ax=ax)
 
-    # Optional: Set title and remove axis labels if needed
-    ax.set_ylabel("")
-    ax.set_xlabel("")
-
-    # Set y axis limits
     ax.set_ylim(0, 0.6)
-
-    # Reduce font size of x and y ticks
     ax.tick_params(axis='both', which='major', labelsize=8)
+    ax.legend(bbox_to_anchor=[0.6, 0.85], loc='center', ncol=2)
 
-    # Remove box around the plot
+    for label in ax.get_xticklabels():
+        if label.get_text() == 'Mean':
+            label.set_fontstyle('italic')
+            label.set_fontweight('bold')
+
+    ax.set_xticklabels(
+        ['Mean\nof all\nproteins' if x.get_text() == 'Mean' else x.get_text() for x in ax.get_xticklabels()])
+
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.spines['left'].set_visible(False)
     ax.spines['bottom'].set_visible(False)
 
-    # Adjust legend position
-    ax.legend(bbox_to_anchor=[0.6, 0.85], loc='center', ncol=2)
-
-    # Change font style of the 'mean' column
-    for label in ax.get_xticklabels():
-        if label.get_text() == 'Mean':
-            label.set_fontstyle('italic')
-            label.set_fontweight('bold')
-    # rename mean column to Mean of all proteins
-    ax.set_xticklabels(
-        ['Mean\nof all\nproteins' if x.get_text() == 'Mean' else x.get_text() for x in ax.get_xticklabels()])
-
-    hue = "Network"
-    hue_order = ["EN", "LGBM"]
+    # Add statistical annotations
     pairs = [
         (("pRB", "EN"), ("pRB", "LGBM")),
         (("CD45", "EN"), ("CD45", "LGBM")),
@@ -133,12 +108,82 @@ def create_bar_plot_en_vs_lgbm(data: pd.DataFrame, metric: str) -> plt.Figure:
         (("pERK", "EN"), ("pERK", "LGBM")),
         (("EGFR", "EN"), ("EGFR", "LGBM")),
         (("ER", "EN"), ("ER", "LGBM")),
-        (("Mean", "EN"), ("Mean", "LGBM")),
+        (("Mean", "EN"), ("Mean", "LGBM"))
     ]
-    order = ['pRB', 'CD45', 'CK19', 'Ki67', 'aSMA', 'Ecad', 'PR', 'CK14', 'HER2', 'AR', 'CK17', 'p21', 'Vimentin',
-             'pERK', 'EGFR', 'ER', "Mean"]
-    annotator = Annotator(ax, pairs, data=data, x="Marker", y=metric, order=order, hue=hue, hue_order=hue_order,
-                          verbose=1)
+    order = SHARED_PROTEINS + ["Mean"]
+    annotator = Annotator(ax, pairs, data=data, x="Marker", y=metric, order=order, hue="Network",
+                          hue_order=["EN", "LGBM"])
+    annotator.configure(test='Mann-Whitney', text_format='star', loc='outside',
+                        comparisons_correction="Benjamini-Hochberg")
+    annotator.apply_and_annotate()
+
+    return ax
+
+
+# Function to plot ARI
+def plot_ari(ax=None):
+    results = pd.read_csv("results/evaluation/cluster_metrics.csv")
+    ax = sns.barplot(data=results, x="Marker", y="ARI", ax=ax)
+    ax.set_ylabel("Adjusted Rand Index")
+    ax.set_xlabel("Marker")
+
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+
+    # rotate x-axis labels
+    for tick in ax.get_xticklabels():
+        tick.set_rotation(45)
+    return ax
+
+
+def plot_silhouette():
+    results = pd.read_csv("results/evaluation/cluster_metrics.csv")
+    # create 30 replicates of the data
+    results = pd.concat([results] * 30, ignore_index=True)
+
+    melt = results.melt(id_vars=["Biopsy", "Marker"],
+                        value_vars=["Silhouette Original", "Silhouette Imputed"],
+                        var_name="Silhouette Type", value_name="Score")
+
+    # rename value Silhouette Original to Original and Silhoutte Imputed to Imputed
+    melt["Silhouette Type"] = melt["Silhouette Type"].replace(
+        {"Silhouette Original": "Original", "Silhouette Imputed": "Imputed"})
+    ax = sns.barplot(data=melt, x="Marker", y="Score", hue="Silhouette Type")
+    ax.set_ylabel("Silhouette Score")
+    ax.set_xlabel("Marker")
+
+    # adjust legend
+    ax.legend(bbox_to_anchor=[0.4, 0.97], loc='center', ncol=2, fontsize=8)
+
+    # rotate x-axis labels
+    for tick in ax.get_xticklabels():
+        tick.set_rotation(45)
+
+    # Add statistical annotations
+    pairs = [
+        (("pRB", "Original"), ("pRB", "Imputed")),
+        (("CD45", "Original"), ("CD45", "Imputed")),
+        (("CK19", "Original"), ("CK19", "Imputed")),
+        (("Ki67", "Original"), ("Ki67", "Imputed")),
+        (("aSMA", "Original"), ("aSMA", "Imputed")),
+        (("Ecad", "Original"), ("Ecad", "Imputed")),
+        (("PR", "Original"), ("PR", "Imputed")),
+        (("CK14", "Original"), ("CK14", "Imputed")),
+        (("HER2", "Original"), ("HER2", "Imputed")),
+        (("AR", "Original"), ("AR", "Imputed")),
+        (("CK17", "Original"), ("CK17", "Imputed")),
+        (("p21", "Original"), ("p21", "Imputed")),
+        (("Vimentin", "Original"), ("Vimentin", "Imputed")),
+        (("pERK", "Original"), ("pERK", "Imputed")),
+        (("EGFR", "Original"), ("EGFR", "Imputed")),
+        (("ER", "Original"), ("ER", "Imputed")),
+    ]
+
+    order = SHARED_PROTEINS
+    annotator = Annotator(ax, pairs, data=melt, x="Marker", y="Score", order=order, hue="Silhouette Type",
+                          hue_order=["Original", "Imputed"])
     annotator.configure(test='Mann-Whitney', text_format='star', loc='outside',
                         comparisons_correction="Benjamini-Hochberg")
     annotator.apply_and_annotate()
@@ -150,138 +195,132 @@ if __name__ == '__main__':
     if not image_folder.exists():
         image_folder.mkdir(parents=True, exist_ok=True)
 
-    # load null model scores
+    # Load and process Null model scores
     null_model_scores = pd.read_csv(f"results/scores/null_model/scores.csv")
-    # rename Protein to Marker
     null_model_scores = null_model_scores.rename(columns={"Protein": "Marker"})
-    # scale MAE scores between 0 and 1
     null_model_scores["MAE"] = (null_model_scores["MAE"] - null_model_scores["MAE"].min()) / (
             null_model_scores["MAE"].max() - null_model_scores["MAE"].min())
 
+    # Load LGBM model scores
     lgbm_scores = pd.read_csv(Path("results", "scores", "lgbm", "scores.csv"))
     lgbm_scores = lgbm_scores[lgbm_scores["FE"] == 0]
-    # select only non hp scores
     lgbm_scores = lgbm_scores[lgbm_scores["HP"] == 0]
-    # rename lgbm scores EXP TO AP
     lgbm_scores["Mode"] = lgbm_scores["Mode"].replace({"EXP": "AP"})
-    # select only AP scores
     lgbm_scores = lgbm_scores[lgbm_scores["Mode"] == "AP"]
-    # sort by marker
     lgbm_scores.sort_values(by=["Marker"], inplace=True)
 
-    # calculate mean performance for each marker and mode
+    # Calculate the mean performance for each marker and mode
     lgbm_mean = lgbm_scores.groupby(["Marker", "Mode", "Biopsy"]).mean(numeric_only=True).reset_index()
-    # calculate the mean of the mean for each mode
     lgbm_mean = lgbm_mean.groupby(["Mode", "Biopsy"]).mean(numeric_only=True).reset_index()
     lgbm_mean["Marker"] = "Mean"
     lgbm_mean["FE"] = 0
     lgbm_mean["HP"] = 1
     lgbm_mean["Network"] = "LGBM"
+    lgbm_scores = pd.concat([lgbm_scores, lgbm_mean], ignore_index=True)
 
-    # add a new row to lgbm scores, adding the mean scores
-    lgbm_scores = lgbm_scores.append(lgbm_mean, ignore_index=True)
-
+    # Load EN model scores
     en_scores = pd.read_csv(Path("results", "scores", "en", "scores.csv"))
     en_scores = en_scores[en_scores["FE"] == 0]
-    # rename EXP to AP
     en_scores["Mode"] = en_scores["Mode"].replace({"EXP": "AP"})
-    # select only AP scores
     en_scores = en_scores[en_scores["Mode"] == "AP"]
-    # sort by marker
     en_scores.sort_values(by=["Marker"], inplace=True)
 
-    # calculate mean performance for each marker and mode
+    # Calculate the mean performance for EN
     en_mean = en_scores.groupby(["Marker", "Mode", "Biopsy"]).mean(numeric_only=True).reset_index()
-    # calculate the mean of the mean for each mode
     en_mean = en_mean.groupby(["Mode", "Biopsy"]).mean(numeric_only=True).reset_index()
     en_mean["Marker"] = "Mean"
     en_mean["FE"] = 0
     en_mean["HP"] = 0
     en_mean["Network"] = "EN"
-    en_scores = en_scores.append(en_mean, ignore_index=True)
+    en_scores = pd.concat([en_scores, en_mean], ignore_index=True)
 
-    # calculate mean performance for each marker and mode
+    # Process Null model for adding Mean row
     null_mean = null_model_scores.groupby(["Model", "Biopsy", "Marker"]).mean(numeric_only=True).reset_index()
-    # calculate mean for each model
     null_mean["Marker"] = "Mean"
     null_mean["FE"] = 0
     null_mean["HP"] = 0
-    null_model_scores = null_model_scores.append(null_mean, ignore_index=True)
+    null_model_scores = pd.concat([null_model_scores, null_mean], ignore_index=True)
 
+    # Combine EN and LGBM scores
     combined_en_lgbm_scores = pd.concat([en_scores, lgbm_scores])
 
+    # Load biopsy data for patients
     bx_data = {}
     for patient in PATIENTS:
-        patient_scores: pd.DataFrame = pd.read_csv(
+        patient_scores = pd.read_csv(
             Path("data", "bxs", "combined", "preprocessed", f"{patient}_excluded_dataset.tsv"), sep="\t")
-
         patient_scores = patient_scores.loc[(patient_scores != 0.0).any(axis=1)]
         bx_data[patient] = patient_scores
 
-    fig = plt.figure(figsize=(10, 8), dpi=300)
-    gspec = fig.add_gridspec(6, 4)
+    # Create the figure with a grid specification
+    fig = plt.figure(figsize=(12, 17), dpi=150)
+    gspec = fig.add_gridspec(8, 4)
 
-    ax11 = fig.add_subplot(gspec[4:6, :1])
-    # remove box from ax1
-    plt.box(False)
-    ax11.text(-0.5, 1.15, "c", transform=ax11.transAxes,
+    # Histograms for each marker
+    ax11 = fig.add_subplot(gspec[4:6, 0])
+    ax11.text(-0.05, 1, "c", transform=ax11.transAxes,
               fontsize=12, fontweight='bold', va='top', ha='right')
-
-    # sns.histplot(gt, color="blue", label="Expression", kde=True)
     hist = sns.histplot(bx_data["9_2"]["CK19"], color="blue", ax=ax11, kde=True, stat="count")
-    hist.set(ylabel="CK19", xlabel=" ")
     sns.histplot(bx_data["9_3"]["CK19"], color="green", ax=ax11, kde=True, stat="count")
     sns.histplot(bx_data["9_14"]["CK19"], color="yellow", ax=ax11, kde=True, stat="count")
     sns.histplot(bx_data["9_15"]["CK19"], color="red", ax=ax11, kde=True, stat="count")
+    ax11.set_ylabel("CK19")
 
-    ax12 = fig.add_subplot(gspec[4:6, 1:2])
-    # remove box from ax1
-    plt.box(False)
-
+    ax12 = fig.add_subplot(gspec[4:6, 1])
     hist = sns.histplot(bx_data["9_2"]["ER"], color="blue", ax=ax12, kde=True, stat="count")
-    hist.set(ylabel="ER", xlabel=" ")
     sns.histplot(bx_data["9_3"]["ER"], color="green", ax=ax12, kde=True, stat="count")
     sns.histplot(bx_data["9_14"]["ER"], color="yellow", ax=ax12, kde=True, stat="count")
     sns.histplot(bx_data["9_15"]["ER"], color="red", ax=ax12, kde=True, stat="count")
+    ax12.set_ylabel("ER")
 
-    ax13 = fig.add_subplot(gspec[4:6, 2:3])
-    # remove box from ax1
-    plt.box(False)
-
+    ax13 = fig.add_subplot(gspec[4:6, 2])
     hist = sns.histplot(bx_data["9_2"]["pRB"], color="blue", ax=ax13, kde=True, stat="count")
-    hist.set(ylabel="pRB", xlabel=" ")
     sns.histplot(bx_data["9_3"]["pRB"], color="green", ax=ax13, kde=True, stat="count")
     sns.histplot(bx_data["9_14"]["pRB"], color="yellow", ax=ax13, kde=True, stat="count")
     sns.histplot(bx_data["9_15"]["pRB"], color="red", ax=ax13, kde=True, stat="count")
+    ax13.set_ylabel("pRB")
 
-    ax14 = fig.add_subplot(gspec[4:6, 3:4])
-    # remove box from ax1
-    plt.box(False)
-
+    ax14 = fig.add_subplot(gspec[4:6, 3])
     hist = sns.histplot(bx_data["9_2"]["CK17"], color="blue", ax=ax14, kde=True, stat="count")
-    hist.set(ylabel="CK17", xlabel=" ")
     sns.histplot(bx_data["9_3"]["CK17"], color="green", ax=ax14, kde=True, stat="count")
     sns.histplot(bx_data["9_14"]["CK17"], color="yellow", ax=ax14, kde=True, stat="count")
     sns.histplot(bx_data["9_15"]["CK17"], color="red", ax=ax14, kde=True, stat="count")
-    plt.legend(labels=["9 2", "9 3", "9 14", "9 15"], loc='upper left', ncol=1, bbox_to_anchor=(0.4, 1.05))
-    # create legend with custom labels
+    ax14.set_ylabel("CK17")
 
+    # First bar plot (Null & EN)
     ax1 = fig.add_subplot(gspec[0:2, :])
-    ax1.text(-0.1, 1.15, "a", transform=ax1.transAxes,
+    ax1.text(-0.05, 1, "a", transform=ax1.transAxes,
              fontsize=12, fontweight='bold', va='top', ha='right')
-    plt.box(False)
-    ax1.set_title('Null & EN MAE', rotation='vertical', x=-0.1, y=0, fontsize=12)
-    ax1 = create_bar_plot_null_model(data=null_model_scores, metric="MAE")
+    ax1.set_title('Null & EN MAE', rotation='vertical', x=-0.05, y=0.25, fontsize=12)
+    ax1 = create_bar_plot_null_model(data=null_model_scores, metric="MAE", ax=ax1)
 
+    # Second bar plot (EN & LGBM)
     ax2 = fig.add_subplot(gspec[2:4, :])
-    ax2.text(-0.1, 1.15, "b", transform=ax2.transAxes,
+    ax2.text(-0.05, 1, "b", transform=ax2.transAxes,
              fontsize=12, fontweight='bold', va='top', ha='right')
-    plt.box(False)
-    ax2.set_title('EN & LGBM MAE', rotation='vertical', x=-0.1, y=0, fontsize=12)
-    ax2 = create_bar_plot_en_vs_lgbm(data=combined_en_lgbm_scores, metric="MAE")
+    ax2.set_title('EN & LGBM MAE', rotation='vertical', x=-0.05, y=0.25, fontsize=12)
+    ax2 = create_bar_plot_en_vs_lgbm(data=combined_en_lgbm_scores, metric="MAE", ax=ax2)
 
+    # Third bar plot (ARI)
+    ax31 = fig.add_subplot(gspec[6:8, :2])
+    ax31.text(-0.05, 1.1, "d", transform=ax31.transAxes,
+              fontsize=12, fontweight='bold', va='top', ha='right')
+
+    ax31 = plot_ari(ax=ax31)
+
+    # Fourth bar plot (Silhouette)
+    ax32 = fig.add_subplot(gspec[6:8, 2:])
+    ax32.text(-0.05, 1.1, "e", transform=ax32.transAxes,
+              fontsize=12, fontweight='bold', va='top', ha='right')
+    ax32 = plot_silhouette()
+
+    plt.box(False)
+
+    # Ensure tight layout to avoid overlap
     plt.tight_layout()
 
+    # Save the figure
     plt.savefig(Path(image_folder, "fig2.png"), dpi=300, bbox_inches='tight')
     plt.savefig(Path(image_folder, "fig2.eps"), dpi=300, bbox_inches='tight', format='eps')
+
     sys.exit()
